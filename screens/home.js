c1d5dcfe4050710c5dae70e59527a2d22c2c8023
bizop <nathan@bizop.io>
2023-09-04 21:42:10 -0600

    Create home.js

---
+++ screens/home.js
@@ -0,0 +1,11 @@
+function HomeScreen() {
+  // Logic for home screen
+  document.getElementById('thing').innerText = 'booo';
+}
+
+function doStuff() {
+  alert('heyo dirt ba!');
+}
+
+// Call the function to ensure it runs
+HomeScreen();
